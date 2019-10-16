4e8a1abeff082b9f7f26c5c7cb4a55c611f05f9e
phatdroid94 <phatdroid94@gmail.com>
2019-10-16 20:59:18 +0700

    feat : create enum ordertype , saucetype

---
+++ app/src/main/java/phamtanphat/ptp/khoaphamtraining/alertdialog04092019/OrderType.java
@@ -0,0 +1,5 @@
+package phamtanphat.ptp.khoaphamtraining.alertdialog04092019;
+
+public enum OrderType {
+    ON_SITE, TAKE_AWAY
+}
---
+++ app/src/main/java/phamtanphat/ptp/khoaphamtraining/alertdialog04092019/SauceType.java
@@ -0,0 +1,5 @@
+package phamtanphat.ptp.khoaphamtraining.alertdialog04092019;
+
+public enum SauceType {
+    SOY_SAUCE, FISH_SAUCE, OLIVE_OIL, KETCHUP, MUSTARD;
+}
