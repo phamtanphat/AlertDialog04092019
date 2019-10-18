50beb1f25ba76283a279a96e4991edf0de120954
phatdroid94 <phatdroid94@gmail.com>
2019-10-18 20:26:15 +0700

    feat : construct alert

--- app/src/main/java/phamtanphat/ptp/khoaphamtraining/alertdialog04092019/MainActivity.java
+++ app/src/main/java/phamtanphat/ptp/khoaphamtraining/alertdialog04092019/MainActivity.java
@@ -2,21 +2,60 @@ package phamtanphat.ptp.khoaphamtraining.alertdialog04092019;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button mBtnAlertDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mBtnAlertDialog = findViewById(R.id.buttonShowAlert);
+//        Order order = new FastFoodOrderBuilder()
+//                        .orderSauce(SauceType.SOY_SAUCE)
+//                        .build();
+        mBtnAlertDialog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder alert = new AlertDialog.Builder(MainActivity.this);
+                alert.setTitle("Ban muon tat app hay khong ?");
+                alert.setMessage("Hay xac thuc ben duoi");
+                alert.setIcon(R.mipmap.ic_launcher);
+                alert.setCancelable(false);
 
-        Order order = new FastFoodOrderBuilder()
-                        .orderSauce(SauceType.SOY_SAUCE)
-                        .build();
-        Log.d("BBB",order.getBreadType().toString());
+                alert.setPositiveButton("Có", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Toast.makeText(MainActivity.this, "Co", Toast.LENGTH_SHORT).show();
+                    }
+                });
+                alert.setNegativeButton("Không", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Toast.makeText(MainActivity.this, "Khong", Toast.LENGTH_SHORT).show();
+                    }
+                });
+                alert.setNeutralButton("Hủy", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Toast.makeText(MainActivity.this, "Hủy", Toast.LENGTH_SHORT).show();
+                    }
+                });
+                alert.show();
+
+            }
+        });
+
+
 
     }
 }
